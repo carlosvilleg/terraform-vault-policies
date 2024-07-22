760ad637f225e66aafbc0604ea2512380e30468e
Carlos Villegas <136353783+carlosvilleg@users.noreply.github.com>
2024-07-22 16:41:45 +0000

    fixed missing slash

--- kv.tf
+++ kv.tf
@@ -1,17 +1,17 @@
 
 # Read the engine policy files from the directory
 data "local_file" "kvv1_policy_files" {
-  for_each = fileset("${path.module}policies/kvv1", "*.hcl.tftpl")
+  for_each = fileset("${path.module}/policies/kvv1", "*.hcl.tftpl")
 
   # Get the file path of each policy file
-  filename = "${path.module}policies/kvv1/${each.key}"
+  filename = "${path.module}/policies/kvv1/${each.key}"
 }
 
 data "local_file" "kvv2_policy_files" {
-  for_each = fileset("${path.module}policies/kvv2", "*.hcl.tftpl")
+  for_each = fileset("${path.module}/policies/kvv2", "*.hcl.tftpl")
 
   # Get the file path of each policy file
-  filename = "${path.module}policies/kvv2/${each.key}"
+  filename = "${path.module}/policies/kvv2/${each.key}"
 }
 
 locals {
@@ -26,7 +26,7 @@ resource "vault_policy" "kvv1-user-policy" {
 	for_each = local.kvv1_secret_policies
 
 	name = "${each.value.engine}/${each.value.secret}-${each.value.basename}"
-	policy = templatefile("${path.module}policies/kvv1/${each.value.basename}.hcl.tftpl", {
+	policy = templatefile("${path.module}/policies/kvv1/${each.value.basename}.hcl.tftpl", {
 		namespace = "${data.vault_namespace.this.path_fq}/"
 		kvv1_engine_path = each.value.engine
 		kvv1_secret_path = each.value.secret
@@ -37,7 +37,7 @@ resource "vault_policy" "kvv2-user-policy" {
 	for_each = local.kvv2_secret_policies
 
 	name = "${each.value.engine}/${each.value.secret}-${each.value.basename}"
-	policy = templatefile("${path.module}policies/kvv2/${each.value.basename}.hcl.tftpl", {
+	policy = templatefile("${path.module}/policies/kvv2/${each.value.basename}.hcl.tftpl", {
 		namespace = "${data.vault_namespace.this.path_fq}/"
 		kvv2_engine_path = each.value.engine
 		kvv2_secret_path = each.value.secret
@@ -49,7 +49,7 @@ resource "vault_policy" "kvv1-app-policy" {
 	namespace = "${data.vault_namespace.this.path_fq}"
 
 	name = "${each.value.engine}/${each.value.secret}-${each.value.basename}"
-	policy = templatefile("${path.module}policies/kvv1/${each.value.basename}.hcl.tftpl", {
+	policy = templatefile("${path.module}/policies/kvv1/${each.value.basename}.hcl.tftpl", {
 		namespace = ""
 		kvv1_engine_path = each.value.engine
 		kvv1_secret_path = each.value.secret
@@ -61,7 +61,7 @@ resource "vault_policy" "kvv2-app-policy" {
 	namespace = "${data.vault_namespace.this.path_fq}"
 
 	name = "${each.value.engine}/${each.value.secret}-${each.value.basename}"
-	policy = templatefile("${path.module}policies/kvv2/${each.value.basename}.hcl.tftpl", {
+	policy = templatefile("${path.module}/policies/kvv2/${each.value.basename}.hcl.tftpl", {
 		namespace = ""
 		kvv2_engine_path = each.value.engine
 		kvv2_secret_path = each.value.secret
