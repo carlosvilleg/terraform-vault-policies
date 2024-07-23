bff6a7f485fe85bb369c9f7508da69e2e4ca5e2a
Carlos Villegas <136353783+carlosvilleg@users.noreply.github.com>
2024-07-23 15:36:50 +0000

    fixed formatting

--- variables.tf
+++ variables.tf
@@ -2,6 +2,6 @@
 variable "namespace_name" {
   type        = string
   description = "Name of the child namespace that will hold the secret engines. Policies will be created at the provider namespace and this child namespace. This value is REQUIRED for secret access policies, but not for standard root and admin namespace policies."
-  default = null
+  default     = null
 }
 
