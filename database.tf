0a901685b308e858152fe9aed3f0bf1f6f83af30
Carlos Villegas <136353783+carlosvilleg@users.noreply.github.com>
2024-07-23 15:37:47 +0000

    corrected names for policies at admin namespace and improved naming of resources in state

--- database.tf
+++ database.tf
@@ -17,16 +17,28 @@ data "local_file" "database_role_policy_files" {
 locals {
   database_engines         = { for a in flatten(try([for k, v in var.database_roles : [for d, r in v : { engine = k, db = d }]], [])) : "${a.engine}-${a.db}" => a }
   database_roles           = { for a in flatten(try([for k, v in var.database_roles : [for d, r in v : [for l in r : { engine = k, role = l, db = d }]]], [])) : "${a.engine}-${a.db}-${a.role}" => a }
-  database_engine_policies = { for a in setproduct(keys(data.local_file.database_engine_policy_files), keys(local.database_engines)) : "${element(a, 1)}-${data.local_file.database_engine_policy_files[element(a, 0)].filename}" => merge({ basename = replace(element(a, 0), ".hcl.tftpl", "") }, local.database_engines[element(a, 1)]) }
-  database_role_policies   = { for a in setproduct(keys(data.local_file.database_role_policy_files), keys(local.database_roles)) : "${element(a, 1)}-${data.local_file.database_role_policy_files[element(a, 0)].filename}" => merge({ basename = replace(element(a, 0), ".hcl.tftpl", "") }, local.database_roles[element(a, 1)]) }
+  database_engine_policies = { for a in setproduct(keys(data.local_file.database_engine_policy_files), keys(local.database_engines)) : "${element(a, 1)}-${element(a, 0)}" => merge({ basename = replace(element(a, 0), ".hcl.tftpl", "") }, local.database_engines[element(a, 1)]) }
+  database_role_policies   = { for a in setproduct(keys(data.local_file.database_role_policy_files), keys(local.database_roles)) : "${element(a, 1)}-${element(a, 0)}" => merge({ basename = replace(element(a, 0), ".hcl.tftpl", "") }, local.database_roles[element(a, 1)]) }
 }
 
 resource "vault_policy" "database-engine-user-policy" {
   for_each = local.database_engine_policies
 
-  name = "${each.value.engine}-${each.value.db}-${each.value.basename}"
+  name = "${data.vault_namespace.this.path_fq}/${each.value.engine}-${each.value.db}-${each.value.basename}"
   policy = templatefile("${path.module}/policies/database-engine/${each.value.basename}.hcl.tftpl", {
     namespace      = "${data.vault_namespace.this.path_fq}/"
+    db_engine_path = each.value.engine
+    connection     = each.value.db
+  })
+}
+
+resource "vault_policy" "database-engine-child-ns-policy" {
+  for_each  = local.database_engine_policies
+  namespace = data.vault_namespace.this.path_fq
+
+  name = "${each.value.engine}-${each.value.db}-${each.value.basename}"
+  policy = templatefile("${path.module}/policies/database-engine/${each.value.basename}.hcl.tftpl", {
+    namespace      = ""
     db_engine_path = each.value.engine
     connection     = each.value.db
   })
@@ -35,7 +47,7 @@ resource "vault_policy" "database-engine-user-policy" {
 resource "vault_policy" "database-role-user-policy" {
   for_each = local.database_role_policies
 
-  name = "${each.value.engine}-${each.value.db}-${each.value.role}-${each.value.basename}"
+  name = "${data.vault_namespace.this.path_fq}/${each.value.engine}-${each.value.db}-${each.value.role}-${each.value.basename}"
   policy = templatefile("${path.module}/policies/database-role/${each.value.basename}.hcl.tftpl", {
     namespace      = "${data.vault_namespace.this.path_fq}/"
     db_engine_path = each.value.engine
--- kv.tf
+++ kv.tf
@@ -17,15 +17,15 @@ data "local_file" "kvv2_policy_files" {
 locals {
   kvv1_secrets         = { for a in flatten(try([for k, v in var.kv_v1_secrets : [for e, s in v : { engine = k, secret = s }]], [])) : "${a.engine}/${a.secret}" => a }
   kvv2_secrets         = { for a in flatten(try([for k, v in var.kv_v2_secrets : [for e, s in v : { engine = k, secret = s }]], [])) : "${a.engine}/${a.secret}" => a }
-  kvv1_secret_policies = { for a in setproduct(keys(data.local_file.kvv1_policy_files), keys(local.kvv1_secrets)) : "${element(a, 1)}-${data.local_file.kvv1_policy_files[element(a, 0)].filename}" => merge({ basename = replace(element(a, 0), ".hcl.tftpl", "") }, local.kvv1_secrets[element(a, 1)]) }
-  kvv2_secret_policies = { for a in setproduct(keys(data.local_file.kvv2_policy_files), keys(local.kvv2_secrets)) : "${element(a, 1)}-${data.local_file.kvv2_policy_files[element(a, 0)].filename}" => merge({ basename = replace(element(a, 0), ".hcl.tftpl", "") }, local.kvv2_secrets[element(a, 1)]) }
+  kvv1_secret_policies = { for a in setproduct(keys(data.local_file.kvv1_policy_files), keys(local.kvv1_secrets)) : "${element(a, 1)}-${element(a, 0)}" => merge({ basename = replace(element(a, 0), ".hcl.tftpl", "") }, local.kvv1_secrets[element(a, 1)]) }
+  kvv2_secret_policies = { for a in setproduct(keys(data.local_file.kvv2_policy_files), keys(local.kvv2_secrets)) : "${element(a, 1)}-${element(a, 0)}" => merge({ basename = replace(element(a, 0), ".hcl.tftpl", "") }, local.kvv2_secrets[element(a, 1)]) }
 }
 
 
 resource "vault_policy" "kvv1-user-policy" {
   for_each = local.kvv1_secret_policies
 
-  name = "${each.value.engine}/${each.value.secret}-${each.value.basename}"
+  name = "${data.vault_namespace.this.path_fq}/${each.value.engine}/${each.value.secret}-${each.value.basename}"
   policy = templatefile("${path.module}/policies/kvv1/${each.value.basename}.hcl.tftpl", {
     namespace        = "${data.vault_namespace.this.path_fq}/"
     kvv1_engine_path = each.value.engine
@@ -36,7 +36,7 @@ resource "vault_policy" "kvv1-user-policy" {
 resource "vault_policy" "kvv2-user-policy" {
   for_each = local.kvv2_secret_policies
 
-  name = "${each.value.engine}/${each.value.secret}-${each.value.basename}"
+  name = "${data.vault_namespace.this.path_fq}/${each.value.engine}/${each.value.secret}-${each.value.basename}"
   policy = templatefile("${path.module}/policies/kvv2/${each.value.basename}.hcl.tftpl", {
     namespace        = "${data.vault_namespace.this.path_fq}/"
     kvv2_engine_path = each.value.engine
